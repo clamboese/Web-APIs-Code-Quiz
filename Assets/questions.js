75ea95eb88564fc2e9243ab7c86b9765f9560060
Cameron Boese <clamboese@gmail.com>
2021-01-04 21:34:38 -0600

    CodeQuizFinal

---
+++ Assets/questions.js
@@ -0,0 +1,80 @@
+var questions = [{
+    question: "What does CSS stand for?",
+    option1: "Compact Style Sheet",
+    option2: "Computer Screen Sheet",
+    option3: "Complex Style Sheet",
+    option4: "Cascading Style Sheet",
+    answer: "3"
+  },
+  {
+    question: "What does HTML stand for?",
+    option1: "Hypertext Markup Language",
+    option2: "How To Make Lots-of-websites",
+    option3: "Hotmail Text Machine Language",
+    option4: "Hyper Thought Made-up Language",
+    answer: "0"
+  },
+  {
+    question: "Which HTML tag us used to define a break in the page?",
+    option1: "<br>",
+    option2: "<css>",
+    option3: "<href>",
+    option4: "<js>",
+    answer: "0"
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    option1: "font",
+    option2: "class",
+    option3: "styles",
+    option4: "style",
+    answer: "3"
+  },
+  {
+    question: "Javascript Code can be called by using",
+    option1: "RMI",
+    option2: "Triggering Event",
+    option3: "PrepProcessor",
+    option4: "Function/Method",
+    answer: "3"
+  },
+  {
+    question: "Correct HTML tag for the largest heading is:",
+    option1: "<head>",
+    option2: "<h6>",
+    option3: "<heading>",
+    option4: "<h1>",
+    answer: "3"
+  },  {
+    question: "The attribute of <form> tag:",
+    option1: "Method",
+    option2: "Action",
+    option3: "Method and Action",
+    option4: "None",
+    answer: "2"
+  },
+  {
+    question: "Which tag creates an unordered list?",
+    option1: "<ol>",
+    option2: "<il>",
+    option3: "<h1>",
+    option4: "<ul>",
+    answer: "3"
+  },
+  {
+    question: "Web pages starts with which of the following tags?",
+    option1: "<body>",
+    option2: "<title>",
+    option3: "<html>",
+    option4: "<form>",
+    answer: "2"
+  },
+  {
+    question: "Which tag creates number/order list?",
+    option1: "<ul>",
+    option2: "<il>",
+    option3: "<ot>",
+    option4: "<ol>",
+    answer: "3"
+  }
+];
